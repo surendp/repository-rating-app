2f3b53e635fc3d4c759ca83e0ba99a0b9e001c56
Surendra Pandey <surendra.pandey2@metropolia.fi>
2020-10-12 16:07:41 +0300

    - Complete exercise 10.2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>I am developing my first react native application</Text>
+      <Text>I am developing my first react native App</Text>
       <StatusBar style="auto" />
     </View>
   );
